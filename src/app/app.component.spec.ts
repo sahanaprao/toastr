09298924712d410d37dd7e5cabaf004939abd466
sahana P <iamsahana.p@gmail.com>
2020-02-10 00:19:30 +0530

    added unit cases

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,24 +1,34 @@
 import { TestBed, async } from '@angular/core/testing';
 import { RouterTestingModule } from '@angular/router/testing';
 import { AppComponent } from './app.component';
+import { ToastrComponent } from './toastr/toastr.component';
+import { CUSTOM_ELEMENTS_SCHEMA, Component } from '@angular/core';
+import { ToastrService } from './toastr/services/toastr.service';
+import { Store } from '@ngrx/store';
+import { Notifications } from './toastr/notifications';
+import { provideMockStore } from '@ngrx/store/testing';
 
 describe('AppComponent', () => {
+
+  let store: Store<Notifications>;
+  const initialState = {  heading: '',
+    subHeading:'',
+    message: '',
+    type: '' };
+
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       imports: [
         RouterTestingModule
       ],
       declarations: [
-        AppComponent
+        AppComponent,
+        ToastrComponent
       ],
+      providers: [provideMockStore({ initialState })],
+      schemas: [CUSTOM_ELEMENTS_SCHEMA], 
     }).compileComponents();
   }));
-
-  it('should create the app', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app).toBeTruthy();
-  });
 
   it(`should have as title 'toastr'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
@@ -26,10 +36,31 @@ describe('AppComponent', () => {
     expect(app.title).toEqual('toastr');
   });
 
-  it('should render title', () => {
+  it('should call getToastr function with min 2 params', () => {
     const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('toastr app is running!');
+    const component = fixture.componentInstance;
+    spyOn(component, "getToastr");
+    component.getToastr('message','heading');
+    // expect(component.getToastr).toHaveBeenCalledWith('message');      // should fail
+    expect(component.getToastr).toHaveBeenCalledWith('message','heading');    //should pass
   });
+
+  it('should spy on service', () => {
+      const fixture = TestBed.createComponent(AppComponent); 
+      const component = fixture.componentInstance;
+      let myMockedService = fixture.debugElement.injector.get(ToastrService);
+      spyOn(myMockedService, 'success');
+      component.getToastr('this is message','header');
+      expect(myMockedService.success);
+  });
+
+//   it('should spy on service message', () => {
+//     let service = TestBed.get(ToastrService);
+//   spyOn(service , "success").and.callThrough();
+//   fixture = TestBed.createComponent(AppComponent);
+//   component = fixture.componentInstance;
+//   fixture.detectChanges();
+//   // expect(myMockedService.success);
+// });
+
 });
